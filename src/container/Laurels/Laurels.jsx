e684ffbdb477c0dce317353b297fd5631be222e2
simicodes <simeonoffiong@gmail.com>
2022-06-27 09:09:52 -0600

    Added bg to the Laurels component

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 import "./Laurels.css";
 
 const Laurels = () => {
-  return <div></div>;
+  return (
+    <div className="app__bg app__wrapper section__padding" id="awards"></div>
+  );
 };
 export default Laurels;
