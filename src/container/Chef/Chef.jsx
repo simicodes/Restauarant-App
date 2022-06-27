53e4d707168f0a6f36ae10f5007affb508426841
simicodes <simeonoffiong@gmail.com>
2022-06-27 07:36:21 -0600

    Chef component added to this project

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
-const Chef = () => (
-  <div>
-    Chef
-  </div>
-);
+import "./Chef.css";
+
+const Chef = () => {
+  return (
+    <div className="app__bg app__wrapper section__padding">
+      <div className="app__wrapper_img app__wrapper_img_reverse">
+        <img src={images.chef} alt="chef" />
+      </div>
+      <div className="app__wrapper_info">
+        <SubHeading title="Chef's Word" />
+        <h1 className="headtext__cormorant">What we believe in</h1>
+
+        <div className="app__chef-content">
+          <div className="app__chef-content_quote">
+            <img src={images.quote} alt="quote" />
+            <p className="p__opensans">
+              Lorem ipsum dolor sit amet consectetur dolorem sit.
+            </p>
+          </div>
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            voluptatum velit mollitia dolorem facilis suscipit cumque, molestias
+            ut ex magni natus laudantium totam quisquam odit consectetur
+            reprehenderit non quae vitae?
+          </p>
+        </div>
+
+        <div className="app__chef-sign">
+          <p>Kevin Luo</p>
+          <p className="p__opensans">Chef & Founder</p>
+          <img src={images.sign} alt="sign" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Chef;
